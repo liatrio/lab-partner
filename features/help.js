4e398b83bd5a1de05517f7bb3a0ee6679e8d3e2c
Chris Schreiber <chriss@liatrio.com>
2020-06-23 13:21:16 -0700

    Split help feature and plugin

---
+++ features/help.js
@@ -0,0 +1,32 @@
+module.exports = async (controller) => {
+    console.log("Enabling feature: Help");
+
+    const botUser = await controller.plugins.slack.whoAmI();
+
+    controller.plugins.help.addCommand(
+        `@${botUser.real_name} help`,
+        ":question: Help",
+        "Display this help message"
+    );
+
+    controller.hears(
+        "help",
+        ["message", "direct_message", "direct_mention", "mention"],
+        async (bot, message) => {
+            const helpMessage = controller.plugins.help.getMessage();
+            console.log("Script help.js: Help triggered");
+            await bot.reply(message, {
+                blocks: [
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: "Howdy :wave:",
+                        },
+                    },
+                    ...helpMessage.blocks,
+                ],
+            });
+        }
+    );
+};
--- plugins/help.js
+++ plugins/help.js
@@ -37,37 +37,4 @@ const help = {
     },
 };
 
-module.exports = async (controller) => {
-    console.log("Enabling feature: Help");
-
-    controller.usePlugin(help);
-
-    const botUser = await controller.plugins.slack.whoAmI();
-
-    controller.plugins.help.addCommand(
-        `@${botUser.real_name} help`,
-        ":question: Help",
-        "Display this help message"
-    );
-
-    controller.hears(
-        "help",
-        ["message", "direct_message", "direct_mention", "mention"],
-        async (bot, message) => {
-            const helpMessage = controller.plugins.help.getMessage();
-            console.log("Script help.js: Help triggered");
-            await bot.reply(message, {
-                blocks: [
-                    {
-                        type: "section",
-                        text: {
-                            type: "mrkdwn",
-                            text: "Howdy :wave:",
-                        },
-                    },
-                    ...helpMessage.blocks,
-                ],
-            });
-        }
-    );
-};
+module.exports = help;
