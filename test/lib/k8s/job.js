a0f070b3a12d30c67759b582bb12fa911b8863a9
Chris Schreiber <chriss@liatrio.com>
2020-08-03 09:40:52 -0700

    remove mocha-eslint

--- test/lib/k8s/job.js
+++ test/lib/k8s/job.js
@@ -1,5 +1,4 @@
-const chai = require("chai");
-const expect = chai.expect;
+const { expect } = require("chai");
 const chance = require("chance").Chance();
 const sinon = require("sinon");
 const MockKubernetesClient = require("../../mocks/kubernetes");
