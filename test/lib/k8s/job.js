aa228dae417605058e766c6408efce8a75af6e6c
Chris Schreiber <chriss@liatrio.com>
2020-08-03 08:46:09 -0700

    add chai as promised in test setup

--- test/lib/k8s/job.js
+++ test/lib/k8s/job.js
@@ -1,6 +1,4 @@
 const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
 const expect = chai.expect;
 const chance = require("chance").Chance();
 const sinon = require("sinon");
