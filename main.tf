452210db15a6b71de159859c50c3c9ca241f006d
JonRudy <jonr@liatrio.com>
2021-04-07 09:54:28 -0700

    adding in helm release to pipeline

--- main.tf
+++ main.tf
@@ -2,6 +2,22 @@ terraform {
   backend "local" {}
 }
 
+provider "helm" {
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
+
 resource "null_resource" "example1" {
   provisioner "local-exec" {
     command = "echo 'Hello World'"
